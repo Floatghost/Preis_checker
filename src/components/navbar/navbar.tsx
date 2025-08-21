076990bed2bb6f1817c280c0cd04c998dd87c2e8
levin <levin.liechti@hotmail.com>
2025-08-21 14:44:07 +0200

    adding fuzzy search and debug view

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -5,9 +5,12 @@ export default function Navbar() {
     return (
         <div className={styles.wrapper}>
             <nav className={styles.navbar}>
-                <h1>Preis Checker</h1>
+                <Link to='/'>
+                    <h1 className={styles.title} >Preis Checker</h1>
+                </Link>
                 <div className={styles.link_wrapper}>
-                    <Link to="/">Home</Link> | <Link to="/stats">Stats</Link>
+                    <Link to="/" className={styles.linkhome}>Home</Link>
+                    <Link to="/stats" className={styles.linkstats}>Stats</Link>
                 </div>
             </nav>
         </div>
