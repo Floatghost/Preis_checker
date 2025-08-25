7ac31c398a758f021a7656be9663d5418da2499c
Loris <loris.monteleone@outlook.com>
2025-08-25 08:07:09 +0200

    tableElem

--- src/components/tableElem/tableElem.tsx
+++ src/components/tableElem/tableElem.tsx
@@ -1,16 +1,25 @@
 import { useState } from "react";
-import styles from "./tableElem.module.css"
+import styles from "./tableElem.module.css";
 
 export default function TableElem() {
-    const [produktName, setProduktName] = useState('');
-    const [produktPreis, setProduktPreis] = useState('');
+  const [produkte, setProdukte] = useState([
+    { name: "Apfel", preis: 2 },
+    { name: "Banane", preis: 1 },
+    { name: "Kaffee", preis: 5 }
+  ]);
 
-    
-    return (
-        <div>
-            <div className={styles.Name}>
-
-            </div>
+  return (
+    <div>
+      {produkte.map((produkt, index) => (
+        <div key={index} className={styles.Linie}>
+          <div className={styles.Name}>
+            <h1>{produkt.name}</h1>
+          </div>
+          <div className={styles.Preis}>
+            <h1>{produkt.preis} €</h1>
+          </div>
         </div>
-    );
-};
+      ))}
+    </div>
+  );
+}
